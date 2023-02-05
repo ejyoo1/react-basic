90870e5f267d48c85a7ea466a399148a07a76971
ejyoo1 <ejyoowork@gmail.com>
2023-02-05 23:29:25 +0900

    NavBar 컴포넌트로 빼내기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import BlogForm from './components/BlogForm';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route path="/" exact>
+            HomePage
+          </Route>
+          <Route path="/blogs">
+            <BlogForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
